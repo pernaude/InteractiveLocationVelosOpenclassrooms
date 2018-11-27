b67dfc0b7a286adb3093542d62c723bfda583fa9
pernaude <pernaude@outlook.com>
2018-11-27 19:56:01 +0100

    Mise à jour du fichier index.php

--- index.php
+++ index.php
@@ -1,218 +1,26 @@
 <?php
-require "server/setpanel.php";
+
 ?><!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8"/>
-<title><?php echo $_bannerset1; ?></title>
+<title></title>
 	    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">   
 
 <link href="https://fonts.googleapis.com/css?family=Raleway:400,500" rel="stylesheet">
-	<meta property="og:url"                content="http://www.duven-fast-loan.com" />
-<meta property="og:type"               content="article" />
-<meta property="og:title"              content="Prêt personnels pour financer vos projets" />
-<meta property="og:description"        content="Demandez nos prêts rapides et fiables pour financer vos projets. Nous offrons des prêts immobiliers, des prets à la consommation, des rachats de crédit, des prêts scolaires pour financer vos études..." />
-<meta property="og:image"              content="http://duven-fast-loan.com/images/set.jpg" />
-	<meta name="keywords" content="immobilier, consommation, rachat de crédit, prêt, particulier, rapide, finance, crédit, confiance, secours, argent, suivi, transfert, compte, emprunter, personnel, rembourser, urgent, monde, sans frais">
-	<meta name="description" content="Le prêt rapide sans frais et à très petits intérêts dans tous les pays du monde. Pas besoin de passer par la banque. Demandez un prêt rapide et sans frais en avance pour réaliser un projet ou résoudre un problème urgent. Nos offres de prêt sont rapides et sécurisées.">
+	<meta property="og:url"                content="" />
+<meta property="og:type"               content="" />
+<meta property="og:title"              content="" />
+<meta property="og:description"        content="" />
+<meta property="og:image"              content="" />
+	<meta name="keywords" content="">
+	<meta name="description" content="">
 <link rel="icon" href="images/favicon.png" type="image/x-icon" />
 <link rel="stylesheet" href="css/default.css?v=<?php echo time(); ?>" type="text/css"/>
 <link rel="stylesheet" href="css/styles.css?v=<?php echo time(); ?>" type="text/css"/>
-<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+<script src="atatic/js/jquery-1.7.2.min.js"></script>
 </head>
 <body>
-<div id="allwrapper">
-<?php include "parts/header.php"; ?>
-<div id="content">
-<div id="theslidingbannercover">
-<div id="theslidingbanner">
-<div class="voltget" id="showforme_3"><a href="demande.php" rel="3" id="slide_3"><div class="page caledsuitter"><span class="bannerinnerset" id="theinnerspan_3"><?php echo $_index_1; ?></span><br/><span class="selectfalsetrgger"><?php echo $_index_4; ?></span></div></a></div><div  class="voltget" id="showforme_2"><a href="demande.php" rel="2" id="slide_2"><div class="page caledsuitter"><span class="bannerinnerset" id="theinnerspan_2"><?php echo $_index_2; ?></span><br/><span class="selectfalsetrgger"><?php echo $_index_4; ?></span></div></a></div><div  class="voltget" id="showforme_1"><a href="demande.php" rel="1" id="slide_1"><div class="page caledsuitter"><span class="bannerinnerset" id="theinnerspan_1"><?php echo $_index_3; ?></span><br/><span class="selectfalsetrgger"><?php echo $_index_4; ?></span></div></a></div>
-</div>
-</div>
 
-<div id="calculateurBox">
-<div class="page">
-<div id="leftContentSimulateur">
-
-
-
-
-
-<h2 class="topsuffersett"><?php echo $_indexx_3; ?></h2>
-<div id="formcoversett">
-<form action="firstsend.php">
-<input type="text" name="amount" id="amountid" placeholder="<?php echo $_indexx_4; ?>" class="placesettsetter" onkeyup="onlydigit(this);" autocomplete="off" maxlength=8 required="required"/>
-<div class="setunderput">* <?php echo $_indexx_5; ?></div>
-<input type="text" name="duree" id="dureeid" placeholder="<?php echo $_indexx_6; ?>" class="placesettsetter" onkeyup="onlydigit(this);" autocomplete="off" maxlength=3 required="required"/>
-<div class="setunderput">* <?php echo $_indexx_7; ?></div>
-<input type="email" name="email" id="emailid" placeholder="<?php echo $_indexx_13; ?>" class="placesettsetter" autocomplete="off" maxlength=100 required="required"/>
-<hr/>
-<div class="thebottomlinearrange"><div class="firstcaseset"><?php echo $_indexx_8; ?></div><div class="secondcaseset"><span id="digitinsideamount">0</span> <?php echo $devisecurrentdefault; ?></div></div>
-<div class="thebottomlinearrange"><div class="firstcaseset"><?php echo $_indexx_9; ?></div><div class="secondcaseset"><span id="digitinsideduree">0</span> <?php echo $_indexx_11; ?></div></div>
-<div class="thebottomlinearrange"><div class="firstcaseset"><?php echo $_indexx_10; ?></div><div class="secondcaseset"><span id="digitinside">0.00</span> <?php echo $devisecurrentdefault; ?></div></div>
-<input type="submit" value="<?php echo $_indexx_12; ?>" class="wideplusGone buttonaccueil"/>
-
-
-</form>
-
-</div>
-</div>
-<div id="rightContentSimulateur">
-Faites une simulation de votre prêt maintenant et profitez de 2 % de taux annuel fixe avec tous nos prêts personnels, nos prêts immobiliers et le regroupement de crédit. Approbation en 24 heures.
-
-</div>
-
-
-
-</div>
-</div>
-
-
-
-
-
-<div id="thenoticeisgood">
-<div class="page">
-<div id="theleftcompanion">
-<img src="images/the2.png"/><br/>
-<h3 class="headerlinersetter"><?php echo $_index_5; ?></h3>
-<p class="thebottomsuit"><?php echo $_index_6; ?></p>
-</div>
-<div id="therightcompanion">
-<img src="images/the3.png"/><br/>
-<h3 class="headerlinersetter"><?php echo $_index_7; ?></h3>
-<p class="thebottomsuit"><?php echo $_index_8; ?></p>
-
-</div>
-<div id="thecentercompanion">
-<img src="images/the1.png"/><br/>
-<h3 class="headerlinersetter"><?php echo $_index_9; ?></h3>
-<p class="thebottomsuit"><?php echo $_index_10; ?></p>
-
-</div>
-
-</div>
-</div>
-<div id="therightdesign">
-<div class="page">
-<div id="thetextcontenet">
-<h3 class="headerlinersetter"><?php echo $_index_11; ?></h3>
-<p class="thebottomsuit">
-<?php echo $_index_12; ?><br/>
-<ul id="superlistegot">
-<li><?php echo $_index_13; ?></li>
-<li><?php echo $_index_14; ?></li>
-<li><?php echo $_index_15; ?></li>
-<li><?php echo $_index_16; ?></li>
-<li><?php echo $_index_17; ?></li>
-<li><?php echo $_index_18; ?></li>
-</ul>
-</p>
-</div>
-<div id="theimagecontener">
-<img src="images/support.png"/>
-</div>
-<div class="clear"></div>
-</div>
-</div>
-
-
-
-
-
-<?php include "parts/partenaires.php"; ?>
-
-</div>
-<?php include "parts/footer.php"; ?>
-</div>
-<script>
-function remettreplace(){
-_screenscrolltop=$(window).scrollTop();
-_headerlong=$("#header").height();
-
-for(i=1;i<=3;i++){
-_slide1contentheight=$("#theinnerspan_"+i).height();
-$("#slide_"+i).css({"paddingTop":((400-_slide1contentheight)/2)+"px"});
-
-}
-if(_screenscrolltop>100){
-	
-}
-
-}
-
-$(window).scroll(function(){
-	
-	
-	
-	
-});
-remettreplace();
-
-_actualsliderbanner=1;
-function bannershow(){
-	if(_actualsliderbanner!=3){
-$("#showforme_"+_actualsliderbanner).fadeOut(500);
-$("#showforme_"+(_actualsliderbanner+1)).fadeIn(500);
-_actualsliderbanner++;
-	}else{
-		
-$("#showforme_"+_actualsliderbanner).fadeOut(500);
-$("#showforme_"+1).fadeIn(500);
-_actualsliderbanner=1;		
-	}
-}
-
-activeshowforbanner=setInterval(bannershow,4000);
-
-
-
-
-
-
-
-function onlydigit(champ)
-{
-var chiffres = new RegExp("[0-9\.\+]"); 
-var verif;
-var points = 0;
-
-for(x = 0; x < champ.value.length; x++)
-{
-verif = chiffres.test(champ.value.charAt(x));
-if(champ.value.charAt(x) == "."){points++;} 
-if(points > 1){verif = false; points = 1;} 
-if(verif == false){champ.value = champ.value.substr(0,x) + champ.value.substr(x+1,champ.value.length-x+1); x--;}
-}
-
-}
-
-function setweldokfit(){
-	var _setamount=parseInt($("#amountid").val().trim()*1);
-var _setduree=parseInt($("#dureeid").val().trim()*1);
-	_montantselect=_setamount;
-_nbmois=_setduree;
-_poucenset=2/100;
-
-_mensualitecalculer=(_montantselect*(_poucenset/12))/(1-Math.pow((1+(_poucenset/12)),-_nbmois));
-_mensualitecalculerfirst=_mensualitecalculer;
-
-
-
-var _setmensualitetotale=((_setamount*2)/100);
-var _setmensualitecalcule=(_setmensualitetotale+_setamount)/_setduree;
-$("#digitinsideamount").html(_setamount);
-$("#digitinsideduree").html(_setduree);
-if(_setamount!==0&&_setduree!==0){
-$("#digitinside").html(_mensualitecalculerfirst.toFixed(2));
-}else{
-$("#digitinside").html("0.00");	
-}
-}
-
-$("#amountid,#dureeid").keyup(function(){
-setweldokfit();
-});
-</script>
 </body>
 </html>
